a50ec64f2e8e6a02c4744aeeb19932ac3a1b8049
jyu <wee.jud@gmail.com>
2013-03-05 15:36:17 +0800

    update custom.el

--- custom.el
+++ custom.el
@@ -39,18 +39,12 @@
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (soothe)))
  '(custom-safe-themes (quote ("ebf6314f5c7c71034ffe2d67db489c6d868be3cf8cdba02e5a936808c5df75c8" default)))
- '(fci-rule-character-color "#202020")
- '(fci-rule-color "#202020")
- '(fringe-mode 4 nil (fringe))
- '(powerline-color1 "#202020")
- '(powerline-color2 "#141414")
  '(session-use-package t nil (session))
- ;; '(tool-bar-mode nil)
- '(whitespace- nil))
+ '(tool-bar-mode nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "nil" :family "CodingFontTobi" :foundry "unknown" :slant normal :weight normal :height 130 :width normal)))))
+ '(default ((t (:background "nil" :family "CodingFontTobi" :foundry "altsys" :slant normal :weight normal :height 120 :width normal)))))
