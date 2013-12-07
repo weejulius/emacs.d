21a24025bb8c137dec29ee41958fe8f0e5eb081e
jyu <wee.jud@gmail.com>
2013-12-07 12:19:19 +0800

    remove some packages

--- custom.el
+++ custom.el
@@ -66,5 +66,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("450b29ed22abeeac279b7eeea592f4eea810105737716fc29807e1684e729c55" "88b663861db4767f7881e5ecff9bb46d65161a20e40585c8128e8bed8747dae5" "73b835431bdbc4e83a3b176a38ebb740fbac78aa2635e1d4827b3c8211e0bc99" "75d4ccc5e912b93f722e57cca3ca1a15e079032cd69fd9bc67268b4c85639663" "99cbc2aaa2b77374c2c06091494bd9d2ebfe6dc5f64c7ccdb36c083aff892f7d" "monokai" default)))
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
+ '(custom-safe-themes (quote ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "450b29ed22abeeac279b7eeea592f4eea810105737716fc29807e1684e729c55" "88b663861db4767f7881e5ecff9bb46d65161a20e40585c8128e8bed8747dae5" "73b835431bdbc4e83a3b176a38ebb740fbac78aa2635e1d4827b3c8211e0bc99" "75d4ccc5e912b93f722e57cca3ca1a15e079032cd69fd9bc67268b4c85639663" "99cbc2aaa2b77374c2c06091494bd9d2ebfe6dc5f64c7ccdb36c083aff892f7d" "monokai" default)))
  '(session-use-package t nil (session)))
